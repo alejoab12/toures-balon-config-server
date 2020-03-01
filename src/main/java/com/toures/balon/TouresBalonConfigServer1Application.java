98ff7c3836d25bc4044143aa89acf091fdabf08c
malcalab <malcalab@everis.com>
2020-03-01 02:58:43 -0500

    se configura el config server

---
+++ src/main/java/com/toures/balon/TouresBalonConfigServer1Application.java
@@ -0,0 +1,14 @@
+package com.toures.balon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class TouresBalonConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouresBalonConfigServer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/toures/balon/TouresBalonConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toures.balon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouresBalonConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
