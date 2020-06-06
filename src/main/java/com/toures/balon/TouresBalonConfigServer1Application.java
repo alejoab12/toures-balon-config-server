55fc6801f732f17d0dc931cb6c1d2c9124902c63
Manuel alejandro alcala <alejoab12@hotmail.com>
2020-06-05 20:35:25 -0500

    Se ajusta para kubernetes

--- src/main/java/com/toures/balon/TouresBalonConfigServer1Application.java
+++ src/main/java/com/toures/balon/TouresBalonConfigServer1Application.java
@@ -2,9 +2,11 @@ package com.toures.balon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
-@SpringBootApplication//
+@SpringBootApplication
+@EnableDiscoveryClient
 public class TouresBalonConfigServer1Application {
 
 	public static void main(String[] args) {
